c88f48d224697aab349678945ae875bf86e2d1f7
Jiya3005 <thakurjiyasingh949@gmail.com>
2026-04-04 03:17:26 +0530

    feat: create landing page with modular component sections

--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -5,8 +5,10 @@ import HowItWorks from "@/components/HowItWorks";
 import MedicationDemo from "@/components/MedicationDemo";
 import FeaturesChess from "@/components/FeaturesChess";
 import FeaturesGrid from "@/components/FeaturesGrid";
+import Timeline from "@/components/Timeline";
 import Stats from "@/components/Stats";
 import Testimonials from "@/components/Testimonials";
+import FaqSection from "@/components/FaqSection";
 import FloatingCta from "@/components/FloatingCta";
 
 const Index = () => {
@@ -19,8 +21,10 @@ const Index = () => {
       <MedicationDemo />
       <FeaturesChess />
       <FeaturesGrid />
+      <Timeline />
       <Stats />
       <Testimonials />
+      <FaqSection />
       <FloatingCta />
     </div>
   );
